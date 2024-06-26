10233dee482178b50bab44701d3463d4760269a4
Jonathan Marshall <jwmarsh16@gmail.com>
2024-06-26 13:37:09 -0500

    lab complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class Dog:
+
+    def __init__(self, name="none", breed="Mutt"):
+        self.name = name
+        self.breed = breed
     pass
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
 class Person:
+    def __init__(self, name="none"):
+        self.name = name
     pass
